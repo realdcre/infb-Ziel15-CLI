d66d28b0f6c79f44a724ff049ab8c3b730e94a3d
dcre <107809443+realdcre@users.noreply.github.com>
2025-01-10 16:49:00 +0100

    first commit
    includes base functionality for main menu, credits and game tutorial
    also implements first drafts of game and player drafts

---
+++ c#-cli/Ziel 15 CLI (C#)/game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ziel_15_CLI__C__
+{
+    internal class game
+    {
+        public int turns { get; set; }
+        public int skips {  get; set; }
+        public int playerAmount { get; set; }
+
+        
+
+        public game()
+        {
+            Console.WriteLine(" You will be presented a random number in each round ");
+            Console.WriteLine(" The choice is yours! Will you keep the number and   ");
+            Console.WriteLine(" add it to your stack that has to be as close to 15  ");
+            Console.WriteLine(" or skip your turn! Be careful though, you can only  ");
+            Console.WriteLine(" skip a certain number of times in the game. The one ");
+            Console.WriteLine(" player closest to 15 at the end of the turns WINS!! ");
+            Thread.Sleep(4000);
+            Console.Clear();
+        }
+
+    }
+}
---
+++ c#-cli/Ziel 15 CLI (C#)/player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ziel_15_CLI__C__
+{
+    internal class player
+    {
+        public string name { get; }
+        public int position { get; set; }
+        public int denyAmount { get; set; }
+        
+        public void deny()
+        {
+            denyAmount++;
+        }
+    }
+}
